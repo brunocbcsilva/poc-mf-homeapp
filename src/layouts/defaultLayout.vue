70953d931af6bde8cbc2f76a4cf18daaf96dc037
bruno.cesar <bruno.cesar@doc88.com.br>
2024-05-09 11:39:26 -0300

    adicionando header component

--- src/layouts/defaultLayout.vue
+++ src/layouts/defaultLayout.vue
@@ -1,6 +1,9 @@
-<script setup></script>
+<script setup>
+import HeaderApplication from '@/components/HeaderApplication.vue'
+</script>
 
 <template>
+  <HeaderApplication />
   <main>
     <slot></slot>
   </main>
